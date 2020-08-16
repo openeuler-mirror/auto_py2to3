7b9ce51ecee502910f2854d93b6e2f5d2a9da4c8
61Duke <loveweihaitong@foxmail.com>
2020-08-16 15:14:37 +0800

    refactoring code structure

---
+++ auto_py2to3/parse/parse.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Huawei Technologies Co., Ltd.
+# A-Tune is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Create: 2020-8-1
+
+
+class ParsePyFiles(object):
+    def __init__(self, file_path):
+        with open(file_path, 'r') as f:
+            self.content = f.readlines()
+
+    def next(self):
+        for con in self.content:
+            yield con
---
+++ auto_py2to3/utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# Copyright (c) 2019 Huawei Technologies Co., Ltd.
+# A-Tune is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Create: 2020-8-1
+
+
+import os
+
+
+def find_all_py_files(path):
+    """
+    :param path:
+    :return:
+    """
+    for base_path, dir_names, file_names in os.walk(path):
+        for real_path in [
+            os.path.join(base_path, p).replace("\\", "/") for p in dir_names + file_names if ".py" in p
+        ]:
+            if real_path:
+                yield real_path
