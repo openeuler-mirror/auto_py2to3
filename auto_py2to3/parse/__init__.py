8b88212de4e21b4e559e42af14b76b44cd644a61
61Duke <loveweihaitong@foxmail.com>
2020-08-17 16:47:40 +0800

    fix some copyright description

--- auto_py2to3/parse/__init__.py
+++ auto_py2to3/parse/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# Copyright (c) 2019 Huawei Technologies Co., Ltd.
-# A-Tune is licensed under the Mulan PSL v2.
+# auto_py2to3is licensed under the Mulan PSL v2.
 # You can use this software according to the terms and conditions of the Mulan PSL v2.
 # You may obtain a copy of Mulan PSL v2 at:
 #     http://license.coscl.org.cn/MulanPSL2
