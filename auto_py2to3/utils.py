dabe87baec7a8cf9f3423c879f704dd015ab4732
61Duke <loveweihaitong@foxmail.com>
2020-08-17 16:50:00 +0800

    fix some copyright description

--- auto_py2to3/utils.py
+++ auto_py2to3/utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-# auto_py2to3is licensed under the Mulan PSL v2.
+# auto_py2to3 is licensed under the Mulan PSL v2.
 # You can use this software according to the terms and conditions of the Mulan PSL v2.
 # You may obtain a copy of Mulan PSL v2 at:
 #     http://license.coscl.org.cn/MulanPSL2
